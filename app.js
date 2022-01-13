e36c2f46747ca83150b28ea67c418dc694c3c3b1
Ashton (Arinze) Ogbonna <ashton@strataconsulting.com>
2022-01-13 18:26:12 +0100

    update: added express

--- app.js
+++ app.js
@@ -1,39 +1,17 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html');
+const PORT = process.env.PORT || 3000;
 
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
+const express = require('express');
+const app = express();
 
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
+app.use(express.static('public'));
 
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
+app.listen(PORT, () => {
+    console.log('Server running at http://127.0.0.1:' + PORT + '/');
+})
 
 // Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
